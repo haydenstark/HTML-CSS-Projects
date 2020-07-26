3a627fdfa7a4fa5f2ba41f71b050300b6e7ce723
Hayden Stark <haydenstark@aol.com>
2020-07-26 16:20:46 -0500

    Basic HTML JS Project

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("This should show up as an alert.")
+
+function My_First_Function() {
+	var string = "Kiss me, I'm Irish!";
+	var result = string.fontcolor("green");
+	document.getElementById("Irish").innerHTML =
+	result;
+}
